80a15c7ece75d9d69926cb0fa6c0ca933cb97717
LuisJann <giannoneluigi10@gmail.com>
2023-03-30 16:27:15 +0200

    completed exercise

--- src/main/java/org/learning/springlamiapizzeriacrud/repository/PizzaRepository.java
+++ src/main/java/org/learning/springlamiapizzeriacrud/repository/PizzaRepository.java
@@ -2,9 +2,13 @@ package org.learning.springlamiapizzeriacrud.repository;
 
 import org.learning.springlamiapizzeriacrud.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
     public List<Pizza> findByNameContainingIgnoreCase(String name);
+
+    @Query(value = "select name from pizzas", nativeQuery = true)
+    public List<String> getName();
 }
